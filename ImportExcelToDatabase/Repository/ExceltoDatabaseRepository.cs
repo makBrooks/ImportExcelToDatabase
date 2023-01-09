0b173964e4be127bfda1f47dcd68bdf9519d708b
Arun <mailtomakarun@gmail.com>
2023-01-09 15:41:40 +0530

    using data table

--- ImportExcelToDatabase/Repository/ExceltoDatabaseRepository.cs
+++ ImportExcelToDatabase/Repository/ExceltoDatabaseRepository.cs
@@ -19,16 +19,41 @@ namespace ImportExcelToDatabase.Repository
 
         public void AddExcelDetails(List<ExceltoDatabase> excel)
         {
-            for (int i = 0; i < excel.Count; i++)
+            DataTable dtMember = new DataTable();
+            dtMember.Columns.Add("slno");
+            dtMember.Columns.Add("name");
+            dtMember.Columns.Add("email");
+            dtMember.Columns.Add("mobile");
+            dtMember.Columns.Add("specialization");
+            if (excel != null && excel.Count > 0)
             {
-                string conn = "server=server6; database=CachDB15;User Id=User15;Password=csmpl#1234;Encrypt=false"; //ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
-                SqlConnection con = new SqlConnection(conn);
-                string query = "  Insert into [CachDB15].[dbo].[ExceltoDatabase] (slno,name,email,mobile,specialization) Values('" + Convert.ToInt32(excel[i].slno) + "','" + excel[i].name.ToString() + "','" + excel[i].email.ToString() + "','" + excel[i].mobile.ToString() + "','" + excel[i].specialization.ToString() + "')";
-                con.Open();
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.ExecuteNonQuery();
-                con.Close();
+                foreach (var items in excel)
+                {
+                    DataRow dr = dtMember.NewRow();
+                    dr["slno"] = items.slno;
+                    dr["name"] = items.name;
+                    dr["email"] = items.email;
+                    dr["mobile"] = items.mobile;
+                    dr["specialization"] = items.specialization;
+                    dtMember.Rows.Add(dr);
+                }
             }
+            //for (int i = 0; i < excel.Count; i++)
+            //{
+            //    string conn = "server=server6; database=CachDB15;User Id=User15;Password=csmpl#1234;Encrypt=false"; //ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            //    SqlConnection con = new SqlConnection(conn);
+            //    string query = "  Insert into [CachDB15].[dbo].[ExceltoDatabase] (slno,name,email,mobile,specialization) Values('" + Convert.ToInt32(excel[i].slno) + "','" + excel[i].name.ToString() + "','" + excel[i].email.ToString() + "','" + excel[i].mobile.ToString() + "','" + excel[i].specialization.ToString() + "')";
+            //    con.Open();
+            //    SqlCommand cmd = new SqlCommand(query, con);
+            //    cmd.ExecuteNonQuery();
+            //    con.Close();
+            //}
+            var cn = _repo.CreateConnection();
+            if (cn.State == ConnectionState.Closed) cn.Open();
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@employeeType",  dtMember, dbType: DbType.Object);
+            cn.Execute("ImportExcelToDatabase", param, commandType: CommandType.StoredProcedure);
+            cn.Close();
         }
 
         public async Task<List<ExceltoDatabaseDetails>> Get()
